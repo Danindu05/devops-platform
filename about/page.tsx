d50dccb8c1fec4bea180cdf809d73033d235cb38
Danindu <179435939+Danindu05@users.noreply.github.com>
2026-04-01 16:29:36 +0530

    feat: about page

---
+++ about/page.tsx
@@ -0,0 +1,19 @@
+ "use client"
+
+import Navbar from "@/components/Navbar"
+
+export default function AboutPage() {
+  return (
+    <main className="bg-white text-gray-900 min-h-screen">
+
+      <Navbar />
+
+      <section className="flex items-center justify-center h-[80vh]">
+        <h1 className="text-5xl md:text-7xl font-extrabold">
+          ABOUT PAGE
+        </h1>
+      </section>
+
+    </main>
+  )
+}
